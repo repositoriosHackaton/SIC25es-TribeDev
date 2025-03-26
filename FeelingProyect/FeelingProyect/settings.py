d272c3e066f8c9d6fe47a72e2b81f1969f9fa5d0
SKsergio <sergiofloress911@gmail.com>
2025-03-26 08:59:53 -0600

    change into the endpoints

--- FeelingProyect/FeelingProyect/settings.py
+++ FeelingProyect/FeelingProyect/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'EchoTribe'
+    'EchoTribe',
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
